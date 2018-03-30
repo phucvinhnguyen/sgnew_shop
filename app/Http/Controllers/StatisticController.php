fd3149214661de92d077f22c1d208677004bf7c5
Nguyen Phuc Vinh/Game Development team/SG <phucvinh.nguyen@nhnent.com>
2018-03-30 17:12:00 +0700

    Develop statistic controller.

--- app/Http/Controllers/StatisticController.php
+++ app/Http/Controllers/StatisticController.php
@@ -2,12 +2,38 @@
 
 namespace App\Http\Controllers;
 
+use App\Repository\Product\ProductRepository;
 use Illuminate\Http\Request;
 
 class StatisticController extends Controller
 {
+    private $customerRepository;
+    private $productRepository;
+
+    public function __construct(ProductRepository $productRepository)
+    {
+        $this->productRepository = $productRepository;
+    }
+
     public function index(Request $request)
     {
-        return view('pages.statistic.index');
+        $fromDate = $request->get('fromDate');
+        $toDate = $request->get('toDate');
+
+        if (empty($toDate) || $toDate > date('Y/m/d')) {
+            $toDate = date('Y/m/d');
+        }
+
+        //$productReportList = $this->productRepository->getProductBetweenDate($fromDate, $toDate);
+
+        dd($this->productRepository->getProductBetweenDate('2018-03-29', '2018-03-30'));
+
+        $productMoneyToday = $this->productRepository->sumReservedPrice(date('Y/m/d'));
+        return view('pages.statistic.index', ['productMoneyToday' => $productMoneyToday]);
+    }
+
+    public function searchDate(Request $request)
+    {
+        return redirect()->route('page.statistic', ['fromDate' => $request->get('time_FromDate'), 'toDate' => $request->get('time_ToDate')]);
     }
 }
--- app/Repository/Product/ProductRepository.php
+++ app/Repository/Product/ProductRepository.php
@@ -11,6 +11,7 @@ namespace App\Repository\Product;
 
 use App\Entity\Product;
 use App\Repository\Base\BaseRepository;
+use Illuminate\Support\Facades\DB;
 
 class ProductRepository extends BaseRepository
 {
@@ -33,5 +34,38 @@ class ProductRepository extends BaseRepository
     public function getByCustomerId($customerId)
     {
         return $this->model->customerId($customerId)->get();
+    }
+
+    public function getProductBetweenDate($fromDate, $toDate)
+    {
+        $query = $this->model;
+
+        $query = $query->select(DB::raw('customer_id, sum(reserved_price) as price, date(created_at) as created_at'));
+
+        if (!empty($fromDate)) {
+            $query = $query->whereRaw('date(created_at) >= \'' .$fromDate. '\'');
+        }
+
+        if (!empty($toDate)) {
+            $query = $query->whereRaw('date(created_at) <= \'' .$toDate. '\'');
+        }
+
+        $query = $query->groupBy(DB::raw('date(created_at)'))->groupBy('customer_id');
+
+        $sub = $query->toSql();
+
+        $records = DB::table(DB::raw("($sub) as a"))
+            ->select(DB::raw('count(customer_id), sum(price), created_at'))
+            ->mergeBindings($query->getQuery())
+            ->groupBy(DB::raw('date(created_at)'));
+
+        dd($records->get());
+
+        return $records;
+    }
+
+    public function sumReservedPrice($date)
+    {
+        return $this->model->select(DB::raw('customer_id, sum(reserved_price) as price'))->whereRaw('date(created_at) = \''. $date .'\'')->groupBy('customer_id')->get();
     }
 }
