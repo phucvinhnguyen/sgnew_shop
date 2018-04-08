10b0c3fc049bdd40aa3b921b027044c247bd00e9
Vinh Nguyễn <phucvinhnguyen@hotmail.com>
2018-04-08 15:04:14 +0700

    Edit and add customer route

--- routes/web.php
+++ routes/web.php
@@ -23,8 +23,11 @@ Route::post('/sale/deleteItem', 'SaleController@deleteCartItem')->name('page.sal
 Route::post('/sale/deleteAll', 'SaleController@deleteAll')->name('page.sale.deleteAll');
 
 Route::get('/customer', 'CustomerController@index')->name('page.customer');
-Route::post('/customer/addInfo', 'CustomerController@createNewCustomerInfo')->name('page.customer.addInfo');
+Route::post('/customer/addInfo', 'CustomerController@addInfoCustomer')->name('page.customer.addInfo');
+Route::post('/customer/deleteInfo', 'CustomerController@deleteInfoCustomer')->name('page.customer.deleteInfo');
 Route::post('/customer/searchCustomer', 'CustomerController@searchCustomer')->name('page.customer.search');
+Route::post('/customer/editPayment', 'CustomerController@editPayment')->name('page.customer.editPayment');
+Route::post('/customer/editCustomer', 'CustomerController@editCustomer')->name('page.customer.editCustomer');
 
 Route::get('/statistic', 'StatisticController@index')->name('page.statistic');
 Route::post('/statistic/searchDate', 'StatisticController@searchDate')->name('page.statistic.searchDate');
