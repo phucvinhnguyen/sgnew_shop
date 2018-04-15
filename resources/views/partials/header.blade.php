03f32fbe85b23c313a761c7a9efa99f6e0215736
Vinh Nguyễn <phucvinhnguyen@hotmail.com>
2018-04-15 14:30:49 +0700

    Update logo

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -3,6 +3,6 @@
         <a class="btn btn-link visible-xs" data-toggle="class:nav-off-screen,open" data-target="#nav,html">
             <i class="fa fa-bars"></i>
         </a>
-        <a href="#" class="navbar-brand" data-toggle="fullscreen"><img src="/images/logo.png" class="m-r-sm">NEW SAIGON</a>
+        <a href="#" class="navbar-brand" data-toggle="fullscreen"><img src="images/logo.png" class="m-r-sm">NEW SAIGON</a>
     </div>
 </header>
